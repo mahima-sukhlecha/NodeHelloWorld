c7096feda25eb77e3c98b790ad4a61a3e0a31c19
MahimaS123 <sukhlecha.mahima45@gmail.com>
2021-08-25 19:37:50 +0530

    add another route

--- server.js
+++ server.js
@@ -2,27 +2,31 @@ var express = require("express"),
   bodyParser = require("body-parser"),
   swaggerJsdoc = require("swagger-jsdoc"),
   swaggerUi = require("swagger-ui-express");
-  swaggerDocument = require('./swagger.json');
+swaggerDocument = require('./swagger.json');
 
 const app = express();
 
 
 
 app.get("/home", (req, res) => {
+  console.log(">>home>>>", req.headers)
+
   res.send("Authentication Flow Works Home Route")
 })
 
-app.get("/home", (req, res) => {
+app.get("/", (req, res) => {
+  console.log(">>>>>", req.headers)
   res.send("Authentication Flow Works")
 })
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-const port = process.env.PORT||3000;
+const port = process.env.PORT || 3000;
 
 //app.get('/home',checkToken , sample)
 
 
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`)})
+  console.log(`Example app listening on port ${port}!`)
+})
