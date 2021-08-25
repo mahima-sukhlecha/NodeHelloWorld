d69cc4dba7345dd017f68aea259e5d9560a3d0d7
MahimaS123 <sukhlecha.mahima45@gmail.com>
2021-08-25 17:30:58 +0530

    add gitignore file and resolve errors

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require("express"),
   bodyParser = require("body-parser"),
   swaggerJsdoc = require("swagger-jsdoc"),
   swaggerUi = require("swagger-ui-express");
-swaggerDocument = require('./swagger.json');
+  swaggerDocument = require('./swagger.json');
 
-
+const app = express();
 
 
 
@@ -14,15 +14,11 @@ app.get("/", (req, res) => {
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-const port = process.env.PORT || 3000;
-
-
-
+const port = process.env.PORT||3000;
 
 //app.get('/home',checkToken , sample)
 
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
-})
+    console.log(`Example app listening on port ${port}!`)})
